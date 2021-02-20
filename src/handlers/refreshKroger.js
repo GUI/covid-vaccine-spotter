210bef466db2f247432b8b076f752539ef49f035
Nick Muerdter <12112+GUI@users.noreply.github.com>
2021-02-19 18:17:05 -0700

    More Kroger attempts.

--- src/handlers/refreshKroger.js
+++ src/handlers/refreshKroger.js
@@ -27,6 +27,126 @@ const RecaptchaOptions = {
 }
 firefox.use(RecaptchaPlugin(RecaptchaOptions))
 
+async function initBrowserPage(browser) {
+  let context;
+  let page;
+
+  await retry(async () => {
+    console.info('Initializing new browser context and page...');
+
+    if (context) {
+      console.info('Closing context and trying again');
+      await context.close();
+    }
+
+    context = await browser.newContext();
+    page = await browser.newPage()
+
+    await page.goto('https://akamai.com', { waitUntil: 'load' });
+    await page.goto('https://www.kingsoopers.com/', { waitUntil: 'load' });
+    await page.waitForSelector('[href="/i/coronavirus-update/vaccine"]');
+    await page.click('[href="/i/coronavirus-update/vaccine"]');
+    await page.waitForLoadState('networkidle');
+    await page.waitForSelector('[href="/rx/covid-eligibility"]');
+    await page.click('[href="/rx/covid-eligibility"]');
+    await page.waitForLoadState('networkidle');
+    if (await page.isVisible('#sec-overlay')) {
+      console.info('OVERLAY!');
+      throw 'Captcha detected';
+    }
+    await retry(async () => {
+      await page.waitForSelector('button[aria-label="I Agree"]');
+      await page.click('button[aria-label="I Agree"]', {
+        delay: _.random(1, 15),
+      });
+      await page.waitForSelector('button[aria-label="No"]:not(.pointer-event-none)', {
+        timeout: 5000,
+      });
+    }, {
+      retries: 3,
+      minTimeout: 5,
+      maxTimeout: 20,
+    });
+    await retry(async () => {
+      await page.click('button[aria-label="No"]:not(.pointer-event-none)', {
+        delay: _.random(1, 15),
+      });
+      await page.waitForSelector('select.kds-Select', {
+        timeout: 5000,
+      });
+    }, {
+      retries: 3,
+      minTimeout: 5,
+      maxTimeout: 20,
+    });
+    await retry(async () => {
+      await page.selectOption('select.kds-Select', 'CO');
+      await page.waitForSelector('button[aria-label="No"]:not(.pointer-event-none)', {
+        timeout: 5000,
+      });
+    }, {
+      retries: 3,
+      minTimeout: 5,
+      maxTimeout: 20,
+    });
+    await retry(async () => {
+      await page.click('button[aria-label="No"]:not(.pointer-event-none)', {
+        delay: _.random(1, 15),
+      });
+      await page.waitForSelector('button[aria-label="No"]:not(.pointer-event-none)', {
+        timeout: 5000,
+      });
+    }, {
+      retries: 3,
+      minTimeout: 5,
+      maxTimeout: 20,
+    });
+    await retry(async () => {
+      await page.click('button[aria-label="No"]:not(.pointer-event-none)', {
+        delay: _.random(1, 15),
+      });
+      await page.waitForSelector('input[placeholder="MM/DD/YYYY"]', {
+        timeout: 5000,
+      });
+      await page.waitForSelector('button[data-qa="BotDatePicker"]', {
+        timeout: 5000,
+      });
+    }, {
+      retries: 3,
+      minTimeout: 5,
+      maxTimeout: 20,
+    });
+    await retry(async () => {
+      await page.fill('input[placeholder="MM/DD/YYYY"]', '01/01/1940');
+      await page.click('button[data-qa="BotDatePicker"]');
+      await page.waitForSelector('button[aria-label="Schedule Your COVID-19 Vaccine"]', {
+        timeout: 5000,
+      });
+    }, {
+      retries: 3,
+      minTimeout: 5,
+      maxTimeout: 20,
+    });
+    await retry(async () => {
+      await page.click('button[aria-label="Schedule Your COVID-19 Vaccine"]');
+      await page.waitForSelector('input[name="location"]');
+    }, {
+      retries: 3,
+      minTimeout: 5,
+      maxTimeout: 20,
+    });
+  }, {
+    retries: 5,
+    minTimeout: 1000,
+    maxTimeout: 5000,
+    onRetry: (err) => {
+      console.info('initBrowserPage Error: ', err);
+    },
+  });
+
+  return { context, page };
+}
+
 module.exports.refreshKroger = async () => {
   const db = await getDatabase();
   const { container } = await db.containers.createIfNotExists({ id: "kroger_stores" });
@@ -39,183 +159,184 @@ module.exports.refreshKroger = async () => {
     devtools: true,
   });
 
+  let { context, page } = await initBrowserPage(browser);
+
   while(true) {
     console.info(`Starting processing: ${new Date()}`);
 
-    const page = await browser.newPage()
+    const processedFacilityIds = {};
+    const processedZipCodes = {};
 
-    await retry(async () => {
-      await page.goto('https://www.kingsoopers.com/rx/covid-vaccine', { waitUntil: 'networkidle' });
-      await page.reload({ waitUntil: 'networkidle' });
+    let { resources } = await container.items
+      .query({
+        query: "SELECT * from c WHERE NOT is_defined(c.lastFetched) OR c.lastFetched <= @minsAgo ORDER BY c.id",
+        parameters: [
+          { name: '@minsAgo', value: DateTime.utc().minus({ minutes: 2 }).toISO() },
+        ],
+      })
+      .fetchAll();
+    resources = _.shuffle(resources);
+    let i = 0;
+    for (const resource of resources) {
+      i++;
+      console.info(`Processing store #${resource.id} (${i} of ${resources.length})...`);
 
-      await page.solveRecaptchas();
-      for (const frame of page.mainFrame().childFrames()) {
-        await frame.solveRecaptchas()
+      if (processedFacilityIds[resource.id]) {
+        console.info(`  Skipping already processed store #${resource.id} as part of earlier request.`);
+        continue;
+      } else if (processedZipCodes[resource.address.zipCode]) {
+        console.info(`  Skipping already processed zip code ${resource.address.zipCode} as part of earlier request.`);
+        continue;
       }
-      if (page.isVisible('#sec-overlay')) {
-        await sleep(1000);
-        page.evaluate('document.getElementById("sec-overlay").style.display = "none";');
-      }
 
-      await page.click('[aria-label="I Agree"]', {
-        delay: _.random(1, 15),
-      });
-      await sleep(1000);
+      const lastFetched = DateTime.utc().toISO();
 
-      const processedFacilityIds = {};
-      const processedZipCodes = {};
-
-      let { resources } = await container.items
-        .query({
-          query: "SELECT * from c WHERE NOT is_defined(c.lastFetched) OR c.lastFetched <= @minsAgo ORDER BY c.id",
-          parameters: [
-            { name: '@minsAgo', value: DateTime.utc().minus({ minutes: 2 }).toISO() },
-          ],
-        })
-        .fetchAll();
-      resources = _.shuffle(resources);
-      let i = 0;
-      for (const resource of resources) {
-        i++;
-        console.info(`Processing store #${resource.id} (${i} of ${resources.length})...`);
-
-        if (processedFacilityIds[resource.id]) {
-          console.info(`  Skipping already processed store #${resource.id} as part of earlier request.`);
-          continue;
-        } else if (processedZipCodes[resource.address.zipCode]) {
-          console.info(`  Skipping already processed zip code ${resource.address.zipCode} as part of earlier request.`);
-          continue;
+      let data = [];
+      let reopen = false;
+      await retry(async () => {
+        console.info('attempt');
+        /*
+        if (reopen) {
+          await page.close();
+          await context.close();
+          ({ context, page } = await initBrowserPage(browser));
+          reopen = false;
         }
+        */
 
-        const lastFetched = DateTime.utc().toISO();
-
-        let data;
-        await retry(async () => {
-          console.info('inner Trying to solve captchas');
+        if (await page.isVisible('#sec-overlay')) {
+          console.info('before recaptcha');
+          await sleep(_.random(100, 500));
+          await page.click('#sec-overlay');
+          await sleep(_.random(100, 500));
+          await page.click('#sec-container');
+          await sleep(_.random(100, 500));
+          await page.click('#sec-overlay');
           await page.solveRecaptchas();
           for (const frame of page.mainFrame().childFrames()) {
             await frame.solveRecaptchas()
           }
-          if (await page.isVisible('#sec-overlay')) {
-            console.info('inner visible');
-            await page.waitForSelector('#sec-overlay', {
-              state: 'hidden',
-            });
-            // await sleep(5000000);
-            // page.evaluate('document.getElementById("sec-overlay").style.display = "none";');
-          }
-
-          data = await page.evaluate(async (options) => {
-            const response = await fetch(`https://www.kingsoopers.com/rx/api/anonymous/scheduler/slots/locationsearch/pharmacy/${options.zipCode}/${options.startDate}/${options.endDate}/50?appointmentReason=122&appointmentReason=125`, {
-              "headers": {
-                "accept": "application/json, text/plain, */*",
-                "cache-control": "no-cache",
-                "pragma": "no-cache",
-                "rx-channel": "WEB",
-                "x-sec-clge-req-type": "ajax"
-              },
-            });
-
-            return await response.json();
-          }, {
-            zipCode: resource.address.zipCode,
-            startDate: startDate.toISODate(),
-            endDate: endDate.toISODate(),
-          });
-
-          if (!Array.isArray(data)) {
-            throw `Data not an array: ${JSON.stringify(data)}`;
-          }
-        }, {
-          retries: 2,
-          onRetry: async (err) => {
-            console.info('inner retry err: ', err);
-          },
-        });
-
-        for (const appointments of data) {
-          const appointmentsStoreId = appointments.loc_no.replace(/^625/, '620');
-          console.info(`  Processing appointment results for store #${appointmentsStoreId}`);
-
-          if (appointments.facilityDetails.address.state !== 'CO') {
-            console.info(`  Skipping appointment result #${appointmentsStoreId} for being out of state: ${appointments.facilityDetails.address.state}.`);
-            continue;
-          }
-
-          const { resource: appointmentsStoreResource } = await container.item(appointmentsStoreId).read();
-          if (!appointmentsStoreResource) {
-            throw `Store not found for ${appointments.loc_no}`;
-          }
-
-          await container.items.upsert({
-            ...appointmentsStoreResource,
-            appointments: appointments,
-            lastFetched,
-          });
-
-          processedFacilityIds[appointmentsStoreId] = true;
-          processedZipCodes[resource.address.zipCode] = true;
-          await sleep(50);
+          console.info('after recaptcha');
         }
 
+        await page.click('input[name="location"]');
+        await page.fill('input[name="location"]', resource.address.zipCode);
+
         await retry(async () => {
-          await page.waitForSelector('[data-testid="SiteMenuContent--CloseButton"]', {
+          await page.click('button.LocationDatePicker-button');
+          await page.click('input[name="location"]');
+          await page.fill('input[name="location"]', resource.address.zipCode);
+          await page.keyboard.press('Enter');
+          await page.waitForLoadState('networkidle');
+          /*
+          await page.waitForSelector('text="Fetching Appointments"');
+          await page.waitForSelector('text="Fetching Appointments"', {
             state: 'hidden',
-            timeout: 5000,
           });
-          await page.click('[data-testid="SiteMenu-HamburgerMenu--Button"]', {
-            delay: _.random(1, 15),
-            timeout: 5000,
-          });
-          await page.waitForSelector('[data-testid="SiteMenuContent--CloseButton"]', {
-            timeout: 5000,
-          });
+          */
         }, {
-          retries: 2,
-        });
-        await sleep(_.random(100, 200));
-        await retry(async () => {
-          await page.click('[data-testid="SiteMenuContent--CloseButton"]', {
-            delay: _.random(1, 15),
-            timeout: 5000,
-          });
-          await page.waitForSelector('[data-testid="SiteMenuContent--CloseButton"]', {
-            state: 'hidden',
-            timeout: 5000,
-          });
-          await page.waitForSelector('[data-testid="SiteMenu-HamburgerMenu--Button"]', {
-            timeout: 5000,
-          });
-        }, {
-          retries: 2,
+          retries: 3,
+          minTimeout: 5,
+          maxTimeout: 20,
         });
 
-        await sleep(1000);
+        // data = await page.evaluate(async (options) => {
+        //   const response = await fetch(`https://www.kingsoopers.com/rx/api/anonymous/scheduler/slots/locationsearch/pharmacy/${options.zipCode}/${options.startDate}/${options.endDate}/50?appointmentReason=122&appointmentReason=125`, {
+        //     "headers": {
+        //       "accept": "application/json, text/plain, */*",
+        //       "cache-control": "no-cache",
+        //       "pragma": "no-cache",
+        //       "rx-channel": "WEB",
+        //       "x-sec-clge-req-type": "ajax"
+        //     },
+        //   });
+
+        //   return await response.json();
+        // }, {
+        //   zipCode: resource.address.zipCode,
+        //   startDate: startDate.toISODate(),
+        //   endDate: endDate.toISODate(),
+        // });
+
+        // if (!Array.isArray(data)) {
+        //   throw `Data not an array: ${JSON.stringify(data)}`;
+        // }
+      }, {
+        retries: 5,
+        minTimeout: 1000,
+        maxTimeout: 5000,
+        onRetry: (err) => {
+          console.info('Data fetch error: ', err);
+          reopen = true;
+        },
+      });
+
+      for (const appointments of data) {
+        const appointmentsStoreId = appointments.loc_no.replace(/^625/, '620');
+        console.info(`  Processing appointment results for store #${appointmentsStoreId}`);
+
+        if (appointments.facilityDetails.address.state !== 'CO') {
+          console.info(`  Skipping appointment result #${appointmentsStoreId} for being out of state: ${appointments.facilityDetails.address.state}.`);
+          continue;
+        }
+
+        const { resource: appointmentsStoreResource } = await container.item(appointmentsStoreId).read();
+        if (!appointmentsStoreResource) {
+          throw `Store not found for ${appointments.loc_no}`;
+        }
+
+        await container.items.upsert({
+          ...appointmentsStoreResource,
+          appointments: appointments,
+          lastFetched,
+        });
+
+        processedFacilityIds[appointmentsStoreId] = true;
+        await sleep(50);
       }
-    }, {
-      retries: 2,
-      onRetry: async (err) => {
-        console.info('outer retry err: ', err);
-        /*
-        console.info('outer Trying to solve captchas');
-        await page.solveRecaptchas();
-        for (const frame of page.mainFrame().childFrames()) {
-          await frame.solveRecaptchas()
-        }
-        await sleep(60000);
-        if (await page.isVisible('#sec-overlay')) {
-          //await sleep(60000);
-          page.evaluate('document.getElementById("sec-overlay").style.display = "none";');
-        }
-        */
-      },
-    });
 
-    await page.close();
+      processedZipCodes[resource.address.zipCode] = true;
+
+      /*
+      await retry(async () => {
+        await page.waitForSelector('[data-testid="SiteMenuContent--CloseButton"]', {
+          state: 'hidden',
+          timeout: 5000,
+        });
+        await page.click('[data-testid="SiteMenu-HamburgerMenu--Button"]', {
+          delay: _.random(1, 15),
+          timeout: 5000,
+        });
+        await page.waitForSelector('[data-testid="SiteMenuContent--CloseButton"]', {
+          timeout: 5000,
+        });
+      }, {
+        retries: 3,
+        minTimeout: 50,
+        maxTimeout: 500,
+      });
+      await sleep(_.random(100, 200));
+      await retry(async () => {
+        await page.click('[data-testid="SiteMenuContent--CloseButton"]', {
+          delay: _.random(1, 15),
+          timeout: 5000,
+        });
+        await page.waitForSelector('[data-testid="SiteMenuContent--CloseButton"]', {
+          state: 'hidden',
+          timeout: 5000,
+        });
+        await page.waitForSelector('[data-testid="SiteMenu-HamburgerMenu--Button"]', {
+          timeout: 5000,
+        });
+      }, {
+        retries: 3,
+        minTimeout: 50,
+        maxTimeout: 500,
+      });
+      */
+    }
 
     console.info(`Finished processing: ${new Date()}`);
-
-    await sleep(_.random(10000, 40000));
   }
 }
 
