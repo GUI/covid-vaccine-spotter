b6b1621ad65d42ce7068233e5c877d7f210dc7e7
Knox <mknox@DESKTOP-T201QVL.localdomain>
2021-02-21 12:55:46 -0700

    Cleanup email sending, add unsubscribe endpoint

--- src/handlers/registerNotification.js
+++ src/handlers/registerNotification.js
@@ -3,6 +3,8 @@ const express = require('express')
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const app = express()
+const sendEmail = require('../sendEmail')
+
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
@@ -11,14 +13,63 @@ app.post('/register', function (req, res) {
     db.containers.createIfNotExists({ id: "notifications" }).then(containerResponse => {
       // TODO actuial verification and parsing out the body
       // TODO delete if no stores selected
-      notification = req.body
-      notification.id = req.body.email
-      containerResponse.container.items.upsert(notification).then(item => {
-        res.send("Success")
+      let emailAddress = req.body.email
+
+      let notification = req.body
+      notification.id = emailAddress
+
+      containerResponse.container.items.upsert(notification).then(itemResponse => {
+        let sendPromise = null
+        if (itemResponse.statusCode == 201){
+          sendPromise = sendEmail(emailAddress,"Verify email address for covid vaccine notifications", `Click this link to confirm you would like to recieve covid vaccine notifications for the following stores.`)
+        } else {
+          sendPromise = sendEmail(emailAddress,"Your covid vaccine notification preferences have been updated.","More details")
+        }
+        // Handle promise's fulfilled/rejected states
+        sendPromise.then(
+          function(data) {
+            console.log(data.MessageId);
+            res.send("Success");
+          }).catch(
+            function(err) {
+              console.error(err, err.stack);
+              res.send("Failed");
+          });
       });
     });
   });
 })
 
-// TODO email verification
+app.get('/unregister', function (req, res) {
+  getDatabase().then(db => {
+    db.containers.createIfNotExists({ id: "notifications" }).then(containerResponse => {
+      // TODO actuial verification and parsing out the body
+      // TODO delete if no stores selected
+      let emailAddress = req.query.email
+
+      containerResponse.container.item(emailAddress).delete().then(() => {
+        console.log(`Unsubscribing ${emailAddress}`)
+        let sendPromise = sendEmail(emailAddress,"Unsubscribed from covid vaccine notifications", `You have been unsubscribed from covid vaccine notifications.`)
+        
+        // Handle promise's fulfilled/rejected states
+        sendPromise.then(
+          function(data) {
+            console.log(data.MessageId);
+            res.send("Success");
+          }).catch(
+            function(err) {
+            console.error(err, err.stack);
+            res.send("Success");
+          });
+      }).catch(
+        function(err) {
+          // Always send success, regardless of if the record was found and deleted
+          console.error(err, err.stack);
+          res.send("Success");
+        }
+      )
+    });
+  });
+})
+
 module.exports.appHandler = serverless(app)
--- src/notify.js
+++ src/notify.js
@@ -1,6 +1,5 @@
 const getDatabase = require('./getDatabase');
-var AWS = require('aws-sdk');
-AWS.config.update({region: 'us-east-2'});
+const sendEmail = require('./sendEmail');
 
 module.exports = async function notify(type,id, store_description, email_details) {
     const db = await getDatabase();
@@ -9,7 +8,7 @@ module.exports = async function notify(type,id, store_description, email_details
     let { resources } = await container.items
       .query({
         // TODO: This is a terrible way to do this for multiple reasons. But with plans to move
-	// to RDS soon this may not be worth investing in right now.
+        // to RDS soon this may not be worth investing in right now.
         // While type is not derived from user input, it is always dangerous
         // to interpolate directly into the query.
         // Also we should be able to find which notifications have this specific store
@@ -21,39 +20,17 @@ module.exports = async function notify(type,id, store_description, email_details
     for (const resource of resources) {
         if ( resource.stores.includes(id) && !notified.includes(resource.email)){
             console.log(`Notifying ${resource.email}`)
-            
-            // Create sendEmail params 
-            var params = {
-                Destination: { /* required */
-                    ToAddresses: [ resource.email ]
-                },
-                Message: { /* required */
-                Body: { /* required */
-                    Text: {
-                    Charset: "UTF-8",
-                    Data: email_details
-                    }
-                },
-                Subject: {
-                    Charset: 'UTF-8',
-                    Data: `Covid Vaccine Appointment Available at ${store_description}!`
-                }
-                },
-                //TODO Fix this email address
-                Source: 'matt@knox.software', /* required */
-            };
-            console.log(params)
-            var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+            let sendPromise = sendEmail(resource.email, `Covid Vaccine Appointment Available at ${store_description}!`,email_details)
 
             // Handle promise's fulfilled/rejected states
             sendPromise.then(
               function(data) {
+                notified.push(resource.email);
                 console.log(data.MessageId);
               }).catch(
                 function(err) {
                 console.error(err, err.stack);
               });
-            notified.push(resource.email)
         }
     }
 }  
