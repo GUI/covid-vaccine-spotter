d71ffc2b9db1228d3f49ea305e389e81de8f223f
Nick Muerdter <12112+GUI@users.noreply.github.com>
2021-03-16 02:08:25 -0600

    Add announcement for new pharmacies.

--- website/components/News.vue
+++ website/components/News.vue
@@ -1,5 +1,21 @@
 <template>
-  <div></div>
+  <div class="alert alert-info" role="alert">
+    <p class="mb-0">
+      <strong>03/16/2021:</strong> 🚀 New pharmacies!
+      <strong>Weis, Winn-Dixie, Harveys, and Fresco y Más</strong> stores are
+      now being scanned for open appointments in all supported states. This
+      currently includes FL, GA, MS, NJ, NY, and PA. Hooray for more options!
+      Any feedback is welcome:
+      <a
+        href="m&#97;ilto&#58;v%&#54;1&#99;&#99;&#105;ne&#64;nic&#107;%6D&#46;org"
+        >vacc&#105;ne&#64;ni&#99;k&#109;&#46;o&#114;&#103;</a
+      >
+      or <a href="https://twitter.com/nickblah">@nickblah</a>. And if you're a
+      coder and have interest in helping out on
+      <a href="https://github.com/GUI/covid-vaccine-finder/issues">GitHub</a>,
+      that would be dandy (although the code's still messy)!
+    </p>
+  </div>
 </template>
 
 <script></script>
