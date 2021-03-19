86427dc7ff8d9a2f1d2be2c57e441965f6aa487b
Nick Muerdter <12112+GUI@users.noreply.github.com>
2021-03-19 07:44:28 -0600

    Add announcement about Walgreens since it still seems to be working.

--- website/components/News.vue
+++ website/components/News.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="alert alert-info" role="alert">
     <p class="mb-0">
-      <strong>03/17/2021:</strong> Good news/bad news. Good news:
-      <strong>Wegmans</strong> locations in PA, NY, and VA are now being
-      scanned! 😃 Bad news: <strong>Walgreens</strong> is now blocking this
-      tool, so no Walgreens data currently. 🙁 I'm trying to fix the Walgreens
-      issue, so I'll keep you posted. Sorry! Any feedback is welcome:
+      <strong>03/19/2021:</strong> Last night I believe I got
+      <strong>Walgreens</strong> scanning working again. Fingers crossed it
+      stays this way! 🤞 Any feedback is welcome:
       <a
         href="m&#97;ilto&#58;v%&#54;1&#99;&#99;&#105;ne&#64;nic&#107;%6D&#46;org"
         >vacc&#105;ne&#64;ni&#99;k&#109;&#46;o&#114;&#103;</a
