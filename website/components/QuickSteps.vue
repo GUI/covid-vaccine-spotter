07095c3a49251bd4de5dfab1d38906e878d66483
Patrick Demers <patrickdemers6@gmail.com>
2021-03-21 14:02:39 -0500

    create QuickSteps component

---
+++ website/components/QuickSteps.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="col-md-6 offset-md-3">
+    <h3>Quick Steps</h3>
+    <ol>
+      <li>Confirm eligibility based on state and local guidelines.</li>
+      <li>
+        Once eligible, use this tool to locate available vaccination
+        appointments. Keep in mind, not all locations offering vaccines are
+        available using this tool.
+      </li>
+      <li>
+        Once you find an appointment time that works for you, visit the booking
+        website (a link will be provided!) and make your appointment.
+      </li>
+    </ol>
+  </div>
+</template>
