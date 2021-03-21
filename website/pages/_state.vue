69a57155ca936f5fa60562c2c265508ce23d8741
Patrick Demers <patrickdemers6@gmail.com>
2021-03-21 14:02:57 -0500

    remove original steps and replace with QuickSteps

--- website/pages/_state.vue
+++ website/pages/_state.vue
@@ -10,76 +10,7 @@
           {{ description }}
         </p>
 
-        <div class="row mb-2">
-          <div class="col-md-6 pb-3">
-            <div class="card card-body h-100 bg-primary text-white shadow-sm">
-              <h2 class="display-6 text-center mb-4">
-                Step 1: Review your county's availability and
-                {{ $store.state.regions.region.metadata.code }}'s eligibility
-              </h2>
-              <template v-if="state.metadata.code === 'CO'">
-                <p class="lead">
-                  Visit
-                  <a
-                    href="https://covid19.colorado.gov/for-coloradans/vaccine/where-can-i-get-vaccinated"
-                    class="text-white"
-                    ><strong class="fw-bold">Colorado.gov</strong></a
-                  >
-                  for detailed information about your county's vaccine options
-                  and review whether or not you are eligible yet.
-                </p>
-                <p class="lead">
-                  You may be able to signup for vaccines with a health care
-                  provider in your area (Kaiser, SCL Health, your county, etc.),
-                  in which case you will be contacted when it's your turn and
-                  you may not need this tool.
-                </p>
-                <a
-                  href="https://covid19.colorado.gov/for-coloradans/vaccine/where-can-i-get-vaccinated"
-                  class="btn btn-light fw-bold fs-5 text-primary"
-                  >Visit Colorado.gov
-                  <font-awesome-icon icon="arrow-alt-circle-right"
-                /></a>
-              </template>
-              <template v-else>
-                <p class="lead">
-                  Be sure to visit your own state's official vaccination website
-                  for detailed information about your county's vaccine options
-                  and review whether or not you are eligible yet.
-                </p>
-                <p class="lead">
-                  You may be able to signup for vaccines with a health care
-                  provider or there may be other options in your area, in which
-                  case you may not need this tool.
-                </p>
-              </template>
-            </div>
-          </div>
-          <div class="col-md-6 pb-3">
-            <div class="card card-body h-100 bg-primary text-white shadow-sm">
-              <h2 class="display-6 text-center mb-4">
-                Step 2: Use this tool to try and find a pharmacy appointment
-              </h2>
-              <p class="lead">
-                If you decide you want to find an appointment at a local
-                pharmacy (and are currently eligible for the vaccine), this tool
-                might be able to help.
-              </p>
-              <p class="lead">
-                Rather than searching around on each pharmacy's website, we'll
-                automatically scan the pharmacy websites and show you any
-                available appointments we can find on one page.
-              </p>
-              <p class="lead">
-                All supported locations in {{ state.metadata.name }} are scanned
-                on a regular basis and this page is updated with any available
-                appointments in the state. If you don't see locations near you
-                right now, appointments can come and go quickly so try visiting
-                the page at different times throughout the day.
-              </p>
-            </div>
-          </div>
-        </div>
+        <quick-steps />
 
         <news />
 
