6c8ad50a44badcd1719a4c330553c87cd7398b36
Nick Muerdter <12112+GUI@users.noreply.github.com>
2021-03-23 01:41:54 -0600

    Update news.

--- website/components/News.vue
+++ website/components/News.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="alert alert-info" role="alert">
     <p class="mb-0">
-      <strong>03/19/2021:</strong> Last night I believe I got
-      <strong>Walgreens</strong> scanning working again. Fingers crossed it
-      stays this way! 🤞 Any feedback is welcome:
+      <strong>03/23/2021:</strong> Once again <strong>Walgreens</strong> has
+      started blocking access, but I'm hoping the latest workarounds should
+      restore Walgreens appointment availability (again). In the meantime, I
+      have also added <strong>Centura Health</strong> appointments for Colorado
+      users. Any feedback is welcome:
       <a
         href="m&#97;ilto&#58;v%&#54;1&#99;&#99;&#105;ne&#64;nic&#107;%6D&#46;org"
         >vacc&#105;ne&#64;ni&#99;k&#109;&#46;o&#114;&#103;</a
