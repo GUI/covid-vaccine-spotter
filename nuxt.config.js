df65a61babedd88c511b24f15e1ca8e9fad17860
Nick Muerdter <12112+GUI@users.noreply.github.com>
2021-05-09 11:12:44 -0600

    Update dependencies.

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,9 +27,9 @@ export default {
       {
         rel: "stylesheet",
         href:
-          "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css",
+          "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css",
         integrity:
-          "sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6",
+          "sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0",
         crossorigin: "anonymous",
       },
     ],
