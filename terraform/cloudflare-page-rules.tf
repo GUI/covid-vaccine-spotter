97d6771d0cb47a253e2cbb6b49b5c5ea883a188a
Nick Muerdter <12112+GUI@users.noreply.github.com>
2021-09-23 22:01:08 -0600

    Eliminate cloudflare workers and extra page rules.
    To reduce page rules to get under free limit and eliminate potential
    costs from workers.

--- terraform/cloudflare-page-rules.tf
+++ terraform/cloudflare-page-rules.tf
@@ -1,29 +1,3 @@
-resource "cloudflare_page_rule" "no-index-html" {
-  zone_id = cloudflare_zone.vaccinespotter-org.id
-  target = "*.vaccinespotter.org/*index.html*"
-  priority = 4
-  status = "active"
-  actions {
-    forwarding_url {
-      status_code = 302
-      url = "https://$1.vaccinespotter.org/$2$3"
-    }
-  }
-}
-
-resource "cloudflare_page_rule" "legacy-all" {
-  zone_id = cloudflare_zone.vaccinespotter-org.id
-  target = "*.vaccinespotter.org/*/all/"
-  priority = 3
-  status = "active"
-  actions {
-    forwarding_url {
-      status_code = 302
-      url = "https://$1.vaccinespotter.org/$2/"
-    }
-  }
-}
-
 resource "cloudflare_page_rule" "www-settings" {
   zone_id = cloudflare_zone.vaccinespotter-org.id
   target = "*.vaccinespotter.org/*"
