342f4b24a04ae061af8a6961352a24fbef891c7b
Nick Muerdter <12112+GUI@users.noreply.github.com>
2021-10-26 18:19:31 -0600

    Revert "Redirect away from pages to shutdown page."
    This reverts commit b2287edce4a31e3a057a638bfe4bc70ccbf4310b.

--- website/pages/US.vue
+++ website/pages/US.vue
@@ -1,16 +1,89 @@
 <template>
-  <div></div>
+  <div>
+    <social-head :title="title" :description="description" />
+
+    <navbar :title="title" :us-state-param="$route.params.state" with-reload />
+
+    <main>
+      <div class="container-lg">
+        <top-content :description="description" />
+
+        <div class="alert alert-danger">
+          This US-wide map view is for testing purposes only. Performance is
+          likely going to be slow to load the map.
+        </div>
+      </div>
+      <div class="container-fluid">
+        <div class="row g-3 g-lg-0">
+          <div class="col-lg-12">
+            <div class="mx-3">
+              <location-map />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container-lg mt-3">
+        <tool-status />
+      </div>
+    </main>
+
+    <footer-content />
+  </div>
 </template>
 
 <script>
 export default {
-  head: {
-    link: [{ rel: "canonical", href: "/shutdown/" }],
-    meta: [
-      { "http-equiv": "refresh", content: "0; url=/shutdown/" },
-      { name: "robots", content: "noindex,follow" },
-      { "http-equiv": "cache-control", content: "no-cache" },
-    ],
+  fetchOnServer: false,
+  fetchDelay: 5,
+
+  async fetch() {
+    try {
+      if (this?.$nuxt?.$loading?.start) {
+        this.$nuxt.$loading.start();
+      }
+
+      const usStatePromise = this.$http.$get(`/api/v0/US.json`);
+
+      const usState = Object.freeze(await usStatePromise);
+
+      this.$store.commit("usStates/set", usState);
+
+      if (this?.$nuxt?.$loading?.finish) {
+        this.$nuxt.$loading.finish();
+      }
+    } catch (error) {
+      if (this.$rollbar) {
+        this.$rollbar.error(error);
+      }
+
+      throw error;
+    }
+  },
+
+  computed: {
+    usState() {
+      return this.$store.state.usStates.usState;
+    },
+
+    usStateCode() {
+      return this.usState?.metadata?.code;
+    },
+
+    usStateName() {
+      return this.usState?.metadata?.name;
+    },
+
+    title() {
+      return this.$t("{state} Vaccine Spotter", {
+        state: this.usStateName || "",
+      });
+    },
+
+    description() {
+      return `A tool to help you track down COVID-19 vaccine appointment openings at ${
+        this.usStateName || ""
+      } pharmacies. Updated every minute.`;
+    },
   },
 };
 </script>
--- website/pages/index.vue
+++ website/pages/index.vue
@@ -1,16 +1,72 @@
 <template>
-  <div></div>
+  <div>
+    <social-head :title="title" :description="description" />
+
+    <navbar :title="title" />
+
+    <main class="container-lg">
+      <top-content :description="description" />
+
+      <p class="lead text-center text-muted my-2 mb-3 sub-lead">
+        {{
+          $t(
+            "Rather than searching around on each pharmacy's website, we'll automatically scan the pharmacy websites and show you any available appointments we can find on one page."
+          )
+        }}
+      </p>
+
+      <news />
+
+      <div class="row row-cols-1 row-cols-md-3 g-3">
+        <div v-for="state in activeStates" :key="state.code" class="col">
+          <NuxtLink
+            :to="localePath(`/${state.code}/`)"
+            class="btn btn-light fw-bold fs-5 text-primary d-block"
+            style="border: 1px solid #ddd"
+            >{{ state.name }} <font-awesome-icon icon="arrow-alt-circle-right"
+          /></NuxtLink>
+          <p class="text-center text-secondary mb-0">
+            <small>{{
+              $t(
+                "Scanning {chain_count} pharmacy chains ({store_count} stores) in {state}",
+                {
+                  chain_count: state.provider_brand_count,
+                  store_count: state.store_count,
+                  state: state.code,
+                  state_name: state.name,
+                }
+              )
+            }}</small>
+          </p>
+        </div>
+      </div>
+    </main>
+
+    <footer-content />
+  </div>
 </template>
 
 <script>
 export default {
-  head: {
-    link: [{ rel: "canonical", href: "/shutdown/" }],
-    meta: [
-      { "http-equiv": "refresh", content: "0; url=/shutdown/" },
-      { name: "robots", content: "noindex,follow" },
-      { "http-equiv": "cache-control", content: "no-cache" },
-    ],
+  async asyncData({ $http }) {
+    const states = Object.freeze(await $http.$get(`/api/v0/states.json`));
+    return { states };
+  },
+
+  data() {
+    return {
+      title: this.$t("Vaccine Spotter"),
+      description: this.$t(
+        "A tool to help you track down COVID-19 vaccine appointment openings at your state's pharmacies. Updated every minute."
+      ),
+      states: [],
+    };
+  },
+
+  computed: {
+    activeStates() {
+      return this.states.filter((state) => state.store_count > 0);
+    },
   },
 };
 </script>
